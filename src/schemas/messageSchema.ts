1617ea3eb82437bfd97fcc0853d9c5ae42fe4599
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-08-22 13:14:27 +0530

    Add user and message schemas with validation using Mongoose and Zod

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,9 @@
+import { Content } from "next/font/google";
+import z from "zod";
+
+export const messageSchema = z.object({
+    content: z
+        .string()
+        .min(10, { message: "Content must be at least 10 characters long" })
+        .max(280, { message: "Content must be at most 280 characters long" })
+}) 
